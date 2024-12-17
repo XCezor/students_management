79f4f0c0d2456a019b5f7120423a4a5ae1ed761c
Szymon Grabarczyk <xcezor@gmail.com>
2024-12-17 09:22:20 +0100

    remove folder and exit program

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from CONSTS import *
 
 def checkPath():
@@ -30,17 +31,33 @@ def renameFolder(old_folder, new_folder):
     for folder in folders:
         if f"Folders/{old_folder}" == folder[0]:
             os.rename(f"Folders/{old_folder}", f"Folders/{new_folder}")
-            return 0
+            print(f"Folder renamed successfully to {new_folder}.\n")
+            return True
     print("Folder with provided name doesn't exist.\n")
+    return False
+
+def removeFolder(folder_name):
+    checkPath()
+    folders = os.walk(FOLDER_PATH)
+    for folder in folders:
+        if f"Folders/{folder_name}" == folder[0]:
+            shutil.rmtree(f"Folders/{folder_name}")
+            print(f"Folder {folder_name} removed successfully.\n")
+            return True
+    print("No such folder has been found.\n")
+    return False
+
 
 while True:
-    option = input("Choose what you want to do:\n1. Create a new folder\n2. Print out all folders\n3. Edit folder name\n").lower()
+    option = input("Choose what you want to do:\n1. Create a new folder\n2. Print out all folders\n3. Edit folder name\n4. Remove folder\n5. Exit\n").lower()
 
     if option == "1" or option == "one":
         folder_name = input("Enter new folder name: ").replace(" ", "_")
         createNewFolder(folder_name)
+
     elif option == "2" or option == "two":
         printFolders()
+
     elif option == "3" or option == "three":
         old_folder = input("Enter folder name you want to rename (empty space will be replaced with underscore '_' sign): ").replace(" ", "_")
         new_folder = input("Enter new name of a folder: ").replace(" ", "_")
@@ -48,5 +65,12 @@ while True:
             print("Names must be different.")
         else:
             renameFolder(old_folder, new_folder)
+
+    elif option == "4" or option == "four":
+        folder_name = input("Enter folder name to remove: ").replace(" ", "_")
+        removeFolder(folder_name)
+
+    elif option == "5" or option == "five" or option == "exit":
+        exit()
     else:
         print("Please input correct number.")
