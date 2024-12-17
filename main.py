c5dba6f1c0dc82c8f5f72ef439aebb22ba21fbf6
Szymon Grabarczyk <xcezor@gmail.com>
2024-12-17 09:12:36 +0100

    edit name

--- main.py
+++ main.py
@@ -24,11 +24,29 @@ def printFolders():
         print(folder[0])
     print("")
 
+def renameFolder(old_folder, new_folder):
+    checkPath()
+    folders = os.walk(FOLDER_PATH)
+    for folder in folders:
+        if f"Folders/{old_folder}" == folder[0]:
+            os.rename(f"Folders/{old_folder}", f"Folders/{new_folder}")
+            return 0
+    print("Folder with provided name doesn't exist.\n")
+
 while True:
-    option = input("Choose what you want to do:\n1. Create a new folder\n2. Print out all folders\n")
+    option = input("Choose what you want to do:\n1. Create a new folder\n2. Print out all folders\n3. Edit folder name\n").lower()
 
     if option == "1" or option == "one":
         folder_name = input("Enter new folder name: ").replace(" ", "_")
         createNewFolder(folder_name)
     elif option == "2" or option == "two":
         printFolders()
+    elif option == "3" or option == "three":
+        old_folder = input("Enter folder name you want to rename (empty space will be replaced with underscore '_' sign): ").replace(" ", "_")
+        new_folder = input("Enter new name of a folder: ").replace(" ", "_")
+        if old_folder == new_folder:
+            print("Names must be different.")
+        else:
+            renameFolder(old_folder, new_folder)
+    else:
+        print("Please input correct number.")
